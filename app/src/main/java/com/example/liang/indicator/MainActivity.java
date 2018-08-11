62de26d851fccc7ad7ed26ddab6d258632dca0ed
xiaoliang <719716279@qq.com>
2018-08-12 00:18:58 +0800

    very cool

---
+++ app/src/main/java/com/example/liang/indicator/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.liang.indicator;
+
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> dataList;
+    private ViewPager vp_content;
+    private LinearLayout ll_indicator;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+        vp_content.setAdapter(new PagerAdapter());
+        for (int i = 0; i <dataList.size()-1 ; i++) {
+            Button button=new Button(this);
+            button.setText("我是猪"+i);
+            button.setBackground(null);
+            ll_indicator.addView(button);
+        }
+    }
+
+    private void initView() {
+         vp_content = (ViewPager)findViewById(R.id.vp_content);
+        ll_indicator = (LinearLayout)findViewById(R.id.ll_indicator);
+    }
+
+    private void initData() {
+        dataList=new ArrayList<String>();
+        for (int i = 0; i <10; i++) {
+            dataList.add("我是猪"+i);
+        }
+    }
+
+    class PagerAdapter extends android.support.v4.view.PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return dataList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view==object;
+        }
+
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            TextView view=new TextView(getApplicationContext());
+            view.setText(dataList.get(position));
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((View) object);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/liang/indicator/MyHorizontalScrollView.java
@@ -0,0 +1,25 @@
+package com.example.liang.indicator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.HorizontalScrollView;
+
+public class MyHorizontalScrollView extends HorizontalScrollView {
+    public MyHorizontalScrollView(Context context) {
+        super(context);
+    }
+
+    public MyHorizontalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyHorizontalScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return true;
+    }
+}
